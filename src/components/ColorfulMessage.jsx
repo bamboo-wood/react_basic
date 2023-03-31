231e7d10ab1a2a50f386e34c81ba7cb163fc966d
bamboo-wood <cantate.1009.0417@gmail.com>
2023-03-31 15:04:30 +0900

    complete

--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
-const ColorfulMessage = (props) => {
+export const ColorfulMessage = (props) => {
   const { color, children } = props;
   const contentStyle = {
     color: color,
     fontSize: "18px",
   };
 
-  return <p style={contentStyle}>{children}</p>;
+  return (
+    <>
+      <p style={contentStyle}>{children}</p>;
+    </>
+  );
 };
-
-export default ColorfulMessage;

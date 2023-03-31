4dd93eac0817d293cb1be7de60db4113328c307b
bamboo-wood <cantate.1009.0417@gmail.com>
2023-03-31 14:56:18 +0900

    use useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickCountUp = () => {
-    setNum(num + 1);
-  };
-
   const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
+  const onClickCountUp = () => setNum(num + 1);
+  const onClickSwitchShowFlag = () => setFaceShowFlag(!faceShowFlag);
+
+  useEffect(() => {
+    if (num % 3 === 0) {
+      faceShowFlag || setFaceShowFlag(true);
+    } else {
+      faceShowFlag && setFaceShowFlag(false);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>hello</h1>
       <ColorfulMessage color="blue">How are you?</ColorfulMessage>
       <ColorfulMessage color="pink">im good</ColorfulMessage>
       <button onClick={onClickCountUp}>Button</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>😃</p>}
     </>
   );
 };
